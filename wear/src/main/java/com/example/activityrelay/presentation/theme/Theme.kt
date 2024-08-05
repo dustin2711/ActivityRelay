390fb571eefdf28f15e3ee493891fcc5353b9cc8
masterdustin <masterdustin@aol.com>
2024-08-06 00:56:32 +0200

    Added AndroidStudio project (with phone link)

---
+++ wear/src/main/java/com/example/activityrelay/presentation/theme/Theme.kt
@@ -0,0 +1,18 @@
+package com.example.activityrelay.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun ActivityRelayTheme(
+    content: @Composable () -> Unit
+)
+{
+    MaterialTheme(
+        colors = wearColorPalette,
+        typography = Typography,
+        // For shapes, we generally recommend using the default Material Wear shapes which are
+        // optimized for round and non-round devices.
+        content = content
+    )
+}
